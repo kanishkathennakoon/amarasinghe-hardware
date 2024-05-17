6370a82661e44e372c0d2ee013dc7c7e3e786bbb
Kanishka Thennakoon <kanishka8880@gmail.com>
2024-05-17 09:52:29 +0530

    Add react router

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,5 +1,6 @@
 import {Navbar, Nav, Container} from 'react-bootstrap';
 import {FaShoppingCart, FaUser} from 'react-icons/fa';
+import {LinkContainer} from 'react-router-bootstrap';
 import logo from '../assets/logo.png'
 
 const Header = () => {
@@ -7,16 +8,21 @@ const Header = () => {
     <header>
         <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
             <Container>
-                <Navbar.Brand href="/">
+                <LinkContainer to='/'>
+                <Navbar.Brand>
                     <img style={{ width: '40px', height: '40px' }} src={logo} alt="Amarasinghe Hardware Logo" />
                     &nbsp;Amarasinghe Hardware
                 </Navbar.Brand>
+                </LinkContainer>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ms-auto">
-                        <Nav.Link href="/cart"><FaShoppingCart /> Cart</Nav.Link>
-                        <Nav.Link href="/login"><FaUser /> Sign in</Nav.Link>
-
+                        <LinkContainer to='/cart'>
+                        <Nav.Link><FaShoppingCart /> Cart</Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to='/login'>
+                        <Nav.Link><FaUser /> Sign in</Nav.Link>
+                        </LinkContainer>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
