786b143d45e12d06dcd9b70368df743aa3526d4c
Kanishka Thennakoon <kanishka8880@gmail.com>
2024-08-09 18:30:36 +0530

    Backend Authentication

--- backend/data/users.js
+++ backend/data/users.js
@@ -1,24 +1,30 @@
-import bcrypt from 'bcryptjs';
+import bcrypt from "bcryptjs";
 
 const users = [
-    {
-        name: 'Admin User',
-        email: 'admin@email.com',
-        password: bcrypt.hashSync('123456', 10),
-        isAdmin: true,
-    },
-    {
-        name: 'Amarasinghe',
-        email: 'amare@email.com',
-        password: bcrypt.hashSync('123456', 10),
-        isAdmin: false,
-    },
-    {
-        name: 'Siril',
-        email: 'siril@email.com',
-        password: bcrypt.hashSync('123456', 10),
-        isAdmin: false,
-    }
+  {
+    name: "Admin User",
+    email: "admin@email.com",
+    password: bcrypt.hashSync("123456", 10),
+    isAdmin: true,
+  },
+  {
+    name: "Amarasinghe",
+    email: "amare@email.com",
+    password: bcrypt.hashSync("123456", 10),
+    isAdmin: false,
+  },
+  {
+    name: "Siril",
+    email: "siril@email.com",
+    password: bcrypt.hashSync("123456", 10),
+    isAdmin: false,
+  },
+  {
+    name: "Kanishka",
+    email: "kanishka@email.com",
+    password: bcrypt.hashSync("123456", 10),
+    isAdmin: false,
+  },
 ];
 
 export default users;
